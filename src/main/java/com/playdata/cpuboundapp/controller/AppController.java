597eafd0551197f6e11b7814e451be3ef5e3b11c
RunKaRin <dlrjswnrla@naver.com>
2023-10-31 11:28:23 +0900

    명령어 추가2

--- src/main/java/com/playdata/cpuboundapp/controller/AppController.java
+++ src/main/java/com/playdata/cpuboundapp/controller/AppController.java
@@ -21,7 +21,7 @@ public class AppController {
 
     @RequestMapping("/hello")
     public String hello() {
-        return "webhook docker test5";
+        return "webhook docker test6";
     }
 
     private String getMD5Digest(String input) throws NoSuchAlgorithmException {
