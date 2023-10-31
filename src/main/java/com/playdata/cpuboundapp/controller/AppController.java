cfc8044d4e348c0662e2dcd5304110664fc07ad1
RunKaRin <dlrjswnrla@naver.com>
2023-10-31 10:48:04 +0900

    이미지 빌트 테스트

--- src/main/java/com/playdata/cpuboundapp/controller/AppController.java
+++ src/main/java/com/playdata/cpuboundapp/controller/AppController.java
@@ -21,7 +21,7 @@ public class AppController {
 
     @RequestMapping("/hello")
     public String hello() {
-        return "webhook docker test";
+        return "webhook docker test2";
     }
 
     private String getMD5Digest(String input) throws NoSuchAlgorithmException {
