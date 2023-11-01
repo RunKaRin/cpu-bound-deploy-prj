831d711a6bf8dd753ddd5997b5a3948939cd39bd
RunKaRin <dlrjswnrla@naver.com>
2023-11-01 10:34:44 +0900

    rolling deploy, sleep 60

--- src/main/java/com/playdata/cpuboundapp/controller/AppController.java
+++ src/main/java/com/playdata/cpuboundapp/controller/AppController.java
@@ -21,7 +21,7 @@ public class AppController {
 
     @RequestMapping("/hello")
     public String hello() {
-        return "rolling deploy";
+        return "rolling deploy, sleep 60";
     }
 
     private String getMD5Digest(String input) throws NoSuchAlgorithmException {
