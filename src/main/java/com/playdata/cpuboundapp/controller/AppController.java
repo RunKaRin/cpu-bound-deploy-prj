b7e0e3425fd9c93fc4078871751120d2a523b5cd
RunKaRin <dlrjswnrla@naver.com>
2023-11-01 10:27:52 +0900

    rolling deploy

--- src/main/java/com/playdata/cpuboundapp/controller/AppController.java
+++ src/main/java/com/playdata/cpuboundapp/controller/AppController.java
@@ -21,7 +21,7 @@ public class AppController {
 
     @RequestMapping("/hello")
     public String hello() {
-        return "next test4";
+        return "rolling deploy";
     }
 
     private String getMD5Digest(String input) throws NoSuchAlgorithmException {
