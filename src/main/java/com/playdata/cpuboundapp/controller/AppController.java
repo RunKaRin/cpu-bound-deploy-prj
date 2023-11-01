3fa135395f1999e00ced07285643ada3c7af9062
RunKaRin <dlrjswnrla@naver.com>
2023-11-01 09:51:42 +0900

    test2

--- src/main/java/com/playdata/cpuboundapp/controller/AppController.java
+++ src/main/java/com/playdata/cpuboundapp/controller/AppController.java
@@ -21,7 +21,7 @@ public class AppController {
 
     @RequestMapping("/hello")
     public String hello() {
-        return "next test2";
+        return "next test3";
     }
 
     private String getMD5Digest(String input) throws NoSuchAlgorithmException {
